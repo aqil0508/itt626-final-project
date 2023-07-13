576b054de176369276b7c0ccd8e9490f2d7fb278
Muhammad Aqil <86609755+aqil0508@users.noreply.github.com>
2023-07-13 08:36:08 +0800

    Update init.js

--- web/src/api/init.js
+++ web/src/api/init.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { rememberToken, getValidToken } from './token'
 
-const baseURL = 'http://localhost:7000'
+const baseURL = 'https://itt626-final-project.vercel.app/'
 
 // Create an axios instance
 const api = axios.create({
