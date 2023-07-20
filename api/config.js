9c1be5e0ba869ece10d8e9b464e6a0c9d1b387ac
Muhammad Aqil <86609755+aqil0508@users.noreply.github.com>
2023-07-20 14:47:55 +0800

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 const serverURL = {
-  port: process.env.PORT || 7000,
-  host: process.env.HOST || 'localhost',
+  port: process.env.PORT || 'https://itt626-final-project-api.vercel.app',
+  host: process.env.HOST || 'https://itt626-final-project.vercel.app',
   serverUrl: () => {
     return `http://${process.env.HOST}:${process.env.PORT}`
   }
